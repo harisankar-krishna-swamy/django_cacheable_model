7bde548dcebdf2cc85b6644e803b37091edb462e
K.Harisankar <harisankar.krishna@outlook.com>
2023-06-21 10:31:27 +0800

    memcached in pipeline

--- src/example_django_project/example_django_project/settings.py
+++ src/example_django_project/example_django_project/settings.py
@@ -130,7 +130,7 @@ CACHE_SET_MANY_LIMIT = 10
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
+        'LOCATION': 'memcached:11211',
     }
 }
 
